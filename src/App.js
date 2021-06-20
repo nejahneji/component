3ea31c28cf0a694d821ccfa3b120775bff1ccb9c
nejahneji <nejinejah6@gmail.com>
2021-06-20 16:38:54 +0100

    chekpointreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import react from "react";
+import Photo from "./Component/Profile/Photo";
+import FullName from "./Component/Profile/FullName";
+import Address from "./Component/Profile/Address";
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+       <Photo className='ph'/>
+       <FullName className='na'/>
+       <Address className='add'/>
+      </div>
     </div>
   );
 }
---
+++ src/Component/Profile/Photo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import superman from './superman.png';
+
+const Photo = () => {
+    return (
+        <div>
+           <img src={superman} alt=''/> 
+        </div>
+    )
+}
+
+export default Photo
